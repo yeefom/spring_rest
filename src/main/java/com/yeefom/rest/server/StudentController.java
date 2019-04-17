07c71daf89f6f27790167076239da303cac3722d
Yifeng Wang <yifengw@opentable.com>
2019-04-17 13:37:24 -0700

    handle exception

--- src/main/java/com/yeefom/rest/server/StudentController.java
+++ src/main/java/com/yeefom/rest/server/StudentController.java
@@ -6,7 +6,11 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import com.yeefom.rest.entity.Student;
+import com.yeefom.rest.entity.StudentErrorResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +35,32 @@ public class StudentController {
 
     @GetMapping("/student/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId < 0 || studentId >= students.size()) {
+            throw new StudentNotFoundException("student id not found: " + studentId);
+        }
+
         return students.get(studentId);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
---
+++ src/main/java/com/yeefom/rest/server/StudentNotFoundException.java
@@ -0,0 +1,15 @@
+package com.yeefom.rest.server;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
